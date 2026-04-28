c575989477ec4e7e200c889bedbbcacf48d3a691
Bruno Cordeiro da Silva <bruno@192.168.0.2>
2026-04-27 21:35:26 -0300

    chore(shorter): exporta ShorterHero e ShorterSuccessState

--- src/features/shorter/components/index.ts
+++ src/features/shorter/components/index.ts
@@ -1,15 +1,10 @@
 /**
  * 🔗 SHORTER COMPONENTS EXPORTS
- * Componentes da feature shorter
  */
-
-// Stats
 export { ShorterStats } from './ShorterStats';
-
-// Form e Estados
+export { ShorterHero } from './ShorterHero';
+export { ShorterSuccessState } from './ShorterSuccessState';
 export { ShorterForm } from './ShorterForm';
 export { RedirectingState } from './RedirectingState';
 export { ErrorAlert } from './ErrorAlert';
-
-// CTA
 export { UpgradeCTA } from './UpgradeCTA';
