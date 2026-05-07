4a7f5b8b5170e3a7880d0cf08924c271686b3e90
Bruno Cordeiro da Silva <bruno@192.168.0.49>
2026-05-07 10:01:32 -0300

    feat(analytics): reorganize geographic tab to hero map layout

--- src/features/analytics/components/geographic/index.ts
+++ src/features/analytics/components/geographic/index.ts
@@ -3,6 +3,7 @@ export { GeographicChart } from "./GeographicChart";
 export { GeographicChoropleth } from "./GeographicChoropleth";
 export { GeographicInsights } from "./GeographicInsights";
 export { GeographicMetrics } from "./GeographicMetrics";
+export { ContinentBreakdown } from "./ContinentBreakdown";
 
 // Hook específico
 export { useGeographicData } from "../../hooks/useGeographicData";
