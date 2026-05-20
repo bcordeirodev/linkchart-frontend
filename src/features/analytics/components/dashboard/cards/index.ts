9efdc3b3e85476e113a4f7da32585399e7b103f3
Bruno Cordeiro da Silva <bruno@192.168.0.31>
2026-05-20 08:49:45 -0300

    feat(dashboard): add UTM source card and social app traffic card

--- src/features/analytics/components/dashboard/cards/index.ts
+++ src/features/analytics/components/dashboard/cards/index.ts
@@ -6,3 +6,5 @@ export { LinkInfoCard } from "./LinkInfoCard";
 export { TimeframeSelector } from "./TimeframeSelector";
 export { ViralityCard } from "./ViralityCard";
 export { TrafficQualityCard } from "./TrafficQualityCard";
+export { UtmSourceCard } from "./UtmSourceCard";
+export { SocialAppCard } from "./SocialAppCard";
