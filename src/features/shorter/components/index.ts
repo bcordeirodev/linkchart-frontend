a8c448ab2e32fc6e61d898151c8bcc23cb1a7a82
Bruno Cordeiro da Silva <bruno@192.168.0.3>
2026-05-27 16:42:11 -0300

    chore(shorter): delete dead components and orphaned types

--- src/features/shorter/components/index.ts
+++ src/features/shorter/components/index.ts
@@ -3,11 +3,6 @@
  */
 export { ShorterStats } from "./ShorterStats";
 export { ShorterHero } from "./ShorterHero";
-export { ShorterSuccessState } from "./ShorterSuccessState";
-export { ShorterForm } from "./ShorterForm";
-export { RedirectingState } from "./RedirectingState";
-export { ErrorAlert } from "./ErrorAlert";
-export { UpgradeCTA } from "./UpgradeCTA";
 export { ShorterSubdomainPromo } from "./ShorterSubdomainPromo";
 export { ShorterHowItWorks } from "./ShorterHowItWorks";
 export { ShorterFaq } from "./ShorterFaq";
